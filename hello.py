3c01f88ff4fb771afbd5261b1e2119214d1d013e
TESTVCTTEAM3SY <chan.numa.1jo@gmail.com>
2024-04-29 16:49:43 +0900

    test 002

--- hello.py
+++ hello.py
@@ -1,1 +1,2 @@
 print("こんにちはPython!")
+print("HelloooooはPython!")
--- imageGetter.py
+++ imageGetter.py
@@ -5,7 +5,6 @@
   ・
   ・
 '''
-
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 import time
